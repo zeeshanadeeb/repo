6c86c12ebbe45419fecbec3c9e31b09d8e0ad1b0
zeeshanadeeb <81961191+zeeshanadeeb@users.noreply.github.com>
2021-04-05 19:04:48 +0530

    Add files via upload

---
+++ JewelleryApp/JewelleryApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using JewelleryApp.Classes;
+using JewelleryApp.View;
+using JewelleryApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JewelleryApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private MainWindowViewModel _mainWindowViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mainWindowViewModel = new MainWindowViewModel();
+            DataContext = _mainWindowViewModel;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _mainWindowViewModel.CurrentView = new LoginViewModel();
+            MainWindowViewModel.MainWindowViewModelObj = _mainWindowViewModel;
+        }
+    }
+}
